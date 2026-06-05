3aad9e2a426a6d5d08145b291080aea9910c2c26
Peter Hayman <peteyhayman@gmail.com>
2026-06-05 11:41:45 +1000

    patch

--- scripts/silver-fox/startup.sh
+++ scripts/silver-fox/startup.sh
@@ -15,13 +15,6 @@ done
 sleep 0.1
 hyprctl dispatch workspace 1
 sleep 0.1
-zed ~/me/beet
-sleep 0.1
-hyprctl dispatch workspace 7
-sleep 0.1
-omarchy-launch-webapp "https://music.youtube.com"
-sleep 0.1
-# not going back to 1?
-hyprctl dispatch workspace 1
-sleep 0.1
+zed ~/me/personal
+sleep 1
 echo "done"
